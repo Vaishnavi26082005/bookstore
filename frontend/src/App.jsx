de2ac3e650d645a9ddb92623c028c66707fd5041
vaishnavi <vaishnavicool1234@gmail.com>
2025-04-16 11:19:09 +0530

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Bannner from './components/Bannner.jsx'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Bannner/>
     </>
   )
 }
